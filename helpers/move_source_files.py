b78e97df3a7b802e7e5c5117dff1200a3da64d5b
RA <rgoodie@users.noreply.github.com>
2025-03-06 12:04:06 -0600

    Change file moving to copying in move_source_files.py

--- helpers/move_source_files.py
+++ helpers/move_source_files.py
@@ -9,8 +9,8 @@ load_dotenv()
 source_dir = os.getenv('SOURCE_DIR')
 destination_dir = './Inbox'
 
-# List of files to move
-files_to_move = [
+# List of files to copy
+files_to_copy = [
     '2020-2021 Season.xlsx',
     '2021-2022 Season.xlsx',
     '2022-2023 Season.xlsx',
@@ -21,16 +21,17 @@ files_to_move = [
 # Ensure the destination directory exists
 if not os.path.exists(destination_dir):
     os.makedirs(destination_dir)
+    
 
-# Move each file from the source directory to the destination directory
-for file_name in files_to_move:
+# Copy each file from the source directory to the destination directory
+for file_name in files_to_copy:
     source_file = os.path.join(source_dir, file_name)
     destination_file = os.path.join(destination_dir, file_name)
     
     try:
-        shutil.move(source_file, destination_file)
-        print(f"Moved: {file_name}")
+        shutil.copy2(source_file, destination_file)
+        print(f"Copied: {file_name}")
     except FileNotFoundError:
         print(f"File not found: {file_name}")
     except Exception as e:
-        print(f"Error moving {file_name}: {e}")
+        print(f"Error copying {file_name}: {e}")
